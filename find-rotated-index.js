20ed2dcf45c1d5fc71e76a11cf50cfd8b9a0d04f
Juan Cruz <jc23729@gmail.com>
2021-03-14 15:02:51 -0400

    find rotated axis

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -6,5 +6,22 @@ function findRotatedIndex(array, num) {
       return binarySearch(array, num, pivot, array.length - 1);
     }
   }
+
+  function binarySearch(array, num, start, end) {
+    if (array.length === 0) return -1;
+    if (num < array[start] || num > array[end]) return -1;
+  
+    while (start <= end) {
+      var mid = Math.floor((start + end) / 2);
+      if (array[mid] === num) {
+        return mid;
+      } else if (num < array[mid]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+    return -1;
+  }
   
 module.exports = findRotatedIndex
