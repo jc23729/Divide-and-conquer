783158e41bf7782ad63540db4cb2570adf5f1d2f
Juan Cruz <jc23729@gmail.com>
2021-03-14 14:47:47 -0400

    update find floor

--- find-floor.js
+++ find-floor.js
@@ -2,9 +2,9 @@ function findFloor(arr, num, low=0, high = arr.length) {
   if (low > high) return -1;
   if (num >= arr[high]) return arr[high];
 
-//   let mid = Math.floor(low + high) / 2)
+  let mid = Math.floor(low + high) / 2)
 
-//   if (arr[mid] ===num) return arr[mid];
+  if (arr[mid] ===num) return arr[mid];
 
 //   if (mid > 0 && arr[mid -1] <=num && num < arr[mid] {
 //       return arr[mid -1];
